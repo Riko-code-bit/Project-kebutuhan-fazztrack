2b775be8921dc9972fb7875092449d4fc5840aa8
riko-lcal <rikohadipratama09@gmail.com>
2023-02-04 01:29:07 +0700

    nambahin note buat bilangan prima.js dan nambahin file collection

---
+++ TugasCollection(belum-selesai).js
@@ -0,0 +1,14 @@
+//input : 1,2,3,4,5
+//output : 10,14
+
+
+let Collection = [1,2,3,4,5]
+let index = Collection
+
+console.log(Collection[index]);
+
+for(let index = 0 ; index < 5; index++) {
+    console.log(Collection[index]);
+}
+
+if ()
--- bilanganPrima.js
+++ bilanganPrima.js
@@ -1,17 +1,24 @@
-var masukkanNilai = (angka) => 
+//Logikanya kan "bilangan prima cuma bisa dibagi bilangan 1 dan bilangan itu sendiri dimana hasilnya 0"
+//berarti kan ada dua pembagi,klo pembaginya ada dua bisa dikatakan bilangan prima (1,10)
+//buat make sure bilangan yang kita inputkan itu hanya punya dua pembagi
+//lakuin looping buat bilangan tersebut untuk dibagi bilangan yang kurang dari dan sama dengan bilangan itu sendiri.
+// berarti kan da tiga variabel yg di pake : nilai input, nilai acuan pembagi = 0,dan nilai utk loopingnya dimulai dari 1 sampai bilangan yg diinput itu sndiri.
+//
+
+const masukkanNilai = (10) //variable input angka
 {
-  let pembaginya = 0;
-  for(let a=1; a <= angka; a++){
-    if(angka%a == 0){
-      pembaginya++
+  let hasilBagi = 0;
+  for(let idx=1; idx <= masukkanNilai; idx++){  
+    if(masukkanNilai%idx == 0){
+      hasilBagi++
     }
   }
-  if(pembaginya == 2){
-    console.log(" adalah bilangan prima")
+  if(hasilBagi == 2){
+    console.log(masukkanNilai + " adalah bilangan prima") //nilai output
   }else{
-    console.log("bukanlah prima")
+    console.log(masukkanNilai + " bukanlah bilangan prima") // nilai output
   }
 }
 
-//input angka disini
-masukkanNilai(79); 
+ 
+ 
