6aae7c5f51ee37292afdd82326cd1a07ebdbf7f1
riko-lcal <rikohadipratama09@gmail.com>
2023-03-15 19:03:45 +0700

    revisi: nambahin assertion chai

---
+++ file latihan sndiri'/tugas-cucumber/features/pageobject/Interaction-page.js
@@ -0,0 +1,37 @@
+import Page from "./page.js"
+
+class InteractionPage extends Page {
+
+    get clickDrop() { 
+        return $(`.show.element-list li:nth-of-type(4)`)
+    }
+
+    // get dropValue() {
+    //     return $(`.ui-state-highlight`)
+    // }
+
+   
+
+
+    async klikDropable() {
+        await this.clickDrop.click()
+    }
+
+    async dragAndDrop1() {
+        const souceElements = $(`#draggable`)
+        // const target = $(`#simpleDropContainer > #droppable`)
+        await souceElements.dragAndDrop({x: 300, y: 50})
+        await browser.pause(2000)
+    }
+
+    async validateCss() {
+
+        const selector = $(`.ui-state-highlight`)
+        const backGround = await selector.getCSSProperty('background-color') 
+        console.log(backGround)
+      
+
+    }
+}
+
+export default new InteractionPage
